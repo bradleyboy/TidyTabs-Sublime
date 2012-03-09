762b948fccf2d9e30ccb1609962dce20421ba5fd
bdaily <brad@slideshowpro.net>
2012-03-08 22:14:37 -0600

    Cleaner check for if a file is active or not, check for scratch status

--- TidyTabs.py
+++ TidyTabs.py
@@ -11,14 +11,14 @@ class TidyTabsCommand(sublime_plugin.WindowCommand):
         for file in self.window.views():
             mtime = os.path.getmtime(file.file_name())
             atime = os.path.getatime(file.file_name())
-            group = self.window.get_view_index(file)[0]
 
             # Do not close the current file of any group
-            if file.file_name() == self.window.active_view_in_group(group).file_name():
+            if file.window() != None:
                 continue
 
             # If file is not dirty and has not been modified in one hour
             # and also has not been accessed in the last minute, close it.
-            if now - mtime > 3600 and now - atime > 60 and not file.is_dirty():
+            if (now - mtime > 3600 and now - atime > 60
+                and not file.is_dirty() and not file.is_scratch()):
                 self.window.focus_view(file)
                 self.window.run_command('close_file')
