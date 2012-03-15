ffd1ac1c0e45e54f7ac0c4bc43b07633c90516ad
bdaily <brad@slideshowpro.net>
2012-03-15 11:00:56 -0500

    If a file no longer exists, leave the tab alone

--- TidyTabs.py
+++ TidyTabs.py
@@ -9,16 +9,18 @@ class TidyTabsCommand(sublime_plugin.WindowCommand):
         now = time.time()
 
         for file in self.window.views():
-            mtime = os.path.getmtime(file.file_name())
-            atime = os.path.getatime(file.file_name())
 
-            # Do not close the current file of any group
-            if file.window() != None:
-                continue
+            if os.path.exists(file.file_name()) == True:
+                mtime = os.path.getmtime(file.file_name())
+                atime = os.path.getatime(file.file_name())
 
-            # If file is not dirty and has not been modified in 30 minutes
-            # and also has not been accessed in the last minute, close it.
-            if (now - mtime > 1800 and now - atime > 60
-                and not file.is_dirty() and not file.is_scratch()):
-                self.window.focus_view(file)
-                self.window.run_command('close_file')
+                # Do not close the current file of any group
+                if file.window() != None:
+                    continue
+
+                # If file is not dirty and has not been modified in 30 minutes
+                # and also has not been accessed in the last minute, close it.
+                if (now - mtime > 1800 and now - atime > 60
+                    and not file.is_dirty() and not file.is_scratch()):
+                    self.window.focus_view(file)
+                    self.window.run_command('close_file')
