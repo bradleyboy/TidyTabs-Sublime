e3ad756a73d40e94110f7ce7aafa211c844c98da
Brad Daily <brad@slideshowpro.net>
2013-09-18 16:33:50 -0500

    ST3 compatibility, add threshold option

--- TidyTabs.py
+++ TidyTabs.py
@@ -7,39 +7,48 @@ import time
 class TidyTabsCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        default_settings = sublime.load_settings(__name__ + ".sublime-settings")
+        default_settings = sublime.load_settings("TidyTabs.sublime-settings")
         default_modified_duration = default_settings.get('tidytabs_modified_duration')
         default_accessed_duration = default_settings.get('tidytabs_accessed_duration')
+        default_threshold = default_settings.get('tidytabs_threshold')
         modified_duration = int(self.window.active_view().settings().get('tidytabs_modified_duration', default_modified_duration))
         accessed_duration = int(self.window.active_view().settings().get('tidytabs_accessed_duration', default_accessed_duration))
+        threshold = int(self.window.active_view().settings().get('tidytabs_threshold', default_threshold))
         now = time.time()
 
-        for file in self.window.views():
+        for x in range(0, self.window.num_groups()):
 
-            path = file.file_name()
+            if (len(self.window.views_in_group(x)) < threshold):
+                break
 
-            '''
-            Close the tab if all of the following are true:
-                * File still exists
-                * File is not the current view in its window
-                * File hasn't been modified in <modified_duration> seconds
-                * File hasn't been accessed in <accessed_duration> seconds
-                * File is not in an unsaved state (dirty)
-                * File is not a scratch (unsaved file)
-            '''
-            if (path
-                and os.path.exists(path) == True
-                and file.window() == None
-                and now - os.path.getmtime(path) > modified_duration
-                and now - os.path.getatime(path) > accessed_duration
-                and not file.is_dirty()
-                and not file.is_scratch()):
-                    self.window.focus_view(file)
-                    self.window.run_command('close_file')
+            for file in self.window.views_in_group(x):      
+                path = file.file_name()
+
+                '''
+                Close the tab if all of the following are true:
+                    * File still exists
+                    * File is not the current view in its window
+                    * File hasn't been modified in <modified_duration> seconds
+                    * File hasn't been accessed in <accessed_duration> seconds
+                    * File is not in an unsaved state (dirty)
+                    * File is not a scratch (unsaved file)
+                '''
+                if (path
+                    and os.path.exists(path) == True
+                    and not file == self.window.active_view_in_group(x)
+                    and now - os.path.getmtime(path) > modified_duration
+                    and now - os.path.getatime(path) > accessed_duration
+                    and not file.is_dirty()
+                    and not file.is_scratch()):
+                        print('CLOSE: ' + path)
+                        self.window.focus_view(file)
+                        self.window.run_command('close_file')
+            
+
 
 class TidyTabsListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        run_on_post_save = sublime.load_settings(__name__ + ".sublime-settings").get('tidytabs_run_on_post_save')
+        run_on_post_save = sublime.load_settings("TidyTabs.sublime-settings").get('tidytabs_run_on_post_save')
         if not run_on_post_save:
             return
         view.window().run_command("tidy_tabs")
